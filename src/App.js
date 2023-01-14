6be42d92a905522dca96c0f1e6514d71427b17cd
sameer52718 <97797606+sameer52718@users.noreply.github.com>
2023-01-14 12:47:10 +0000

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { About , Header , Footer , Skills , Testimonial , Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 export const App = () => {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />
